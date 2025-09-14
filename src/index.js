ad06b2a7a81eb8acc1724edb0fd9f6685ba86965
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-09-14 15:20:48 +0200

    Add event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+const create_project_btn = document.querySelector("#create-project-btn");
+const createProjectDialog = document.querySelector("#createProjectDialog");
+const cancelProjectBtn = createProjectDialog.querySelector("#cancelProjectBtn");
+
+cancelProjectBtn.addEventListener("click", () => {
+  createProjectDialog.close();
+});
+
+create_project_btn.addEventListener("click", () => {
+  createProjectDialog.showModal();
+});
